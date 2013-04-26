10216d69347f97dd604da065d04cb0e431048b3b
zsummer <zhangyawei@zhangyawei.ultizen.com>
2013-04-26 11:34:42 +0800

    update log

--- log4z.h
+++ log4z.h
@@ -40,7 +40,7 @@
  * VERSION:  2.0.0
  * PURPOSE:  A lightweight library for error reporting and logging to file and screen .
  * CREATION: 2010.10.4
- * LCHANGE:  2013.04.23
+ * LCHANGE:  2013.04.25
  * LICENSE:  Expat/MIT License, See Copyright Notice at the begin of this file.
  */
 
@@ -53,11 +53,11 @@
  */
 
 
-/*
+/* 
  * UPDATES LOG
- *
+ * 
  * VERSION 0.1.0 <DATE: 2010.10.4>
- *    create the first project.  
+ * 	create the first project.  
  * 	It support put log to screen and files, 
  * 	support log level, support one day one log file.
  * 	support multiple thread, multiple operating system.
@@ -74,34 +74,33 @@
  * 	support comments in the config file.
  * 	add a advanced demo in the ./project
  * 	fix some details.
- *
+ * 
  * VERSION 1.0.1 <DATE: 2013.01.01>
- *    the source code haven't any change.
- *	   fix some Comments in the log4z
- *	   add some comments in the test projects.
- *	   delete some needless code in the 'fast_test' demo projects, it's so simple.
- *
+ * 	the source code haven't any change.
+ * 	fix some Comments in the log4z
+ * 	add some comments in the test projects.
+ * 	delete some needless code in the 'fast_test' demo projects, it's so simple.
+ * 
  * VERSION 1.1.0 <DATE: 2013.01.24>
- *    the method Start will wait for the logger thread started.
- *	   config and add method change. 
- *	   namespace change.
- *	   append some macro.
- *
+ * 	the method Start will wait for the logger thread started.
+ * 	config and add method change. 
+ * 	namespace change.
+ * 	append some macro.
+ * 
  * VERSION 1.1.1 <DATE: 2013.02.23>
- *    add GetStatus**** mothed.
- *	   optimize. 
+ * 	add GetStatus**** mothed.
+ * 	optimize. 
  * VERSION 1.2.0 <DATE: 2013.04.05>
- *    optimize log macro.
- *
+ * 	optimize log macro.
+ * 
  * VERSION 1.2.1 <DATE: 2013.04.13>
- *    1.20 optimize detail fixed.
- *
+ * 	1.20 optimize detail fixed.
+ * 
  * VERSION 2.0.0 <DATE: 2013.04.25>
- *    optimize interface.
- *    change config file format.
- *    file name suffix add process id.
- *	  main logger can config.
- *
+ * 	new interface.
+ * 	new config design.
+ * 	file name append process id.
+ * 
  */
 
 #pragma once
