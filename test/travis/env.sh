179cc147ea7e827aba9f2e8f15b813ca9a5d7e96
zsummer <yawei.zhang@foxmail.com>
2015-11-21 17:54:29 +0800

    update travis

--- test/travis/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
-  echo "osx system" 
-else
-  echo "linux system" `whoami`
-  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test 
-  sudo apt-get update -qq
-fi
-
