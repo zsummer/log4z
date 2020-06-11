d510611e1a19affdd7987fe177c879e240cf03d6
zsummer <yawei.zhang@foxmail.com>
2020-06-11 20:27:06 +0800

    auto make script

---
+++ make_win32.bat
@@ -0,0 +1,14 @@
+
+rd /Q /S vs_sln 
+md vs_sln
+cd vs_sln
+del ./
+cmake ../ -DCMAKE_BUILD_TYPE=Release -A x64 -DCMAKE_GENERATOR_PLATFORM=x64 
+MSBuild log4z.sln /property:Configuration=Release /property:Platform=x64
+mv ../lib/Release/* ../lib/
+rd /Q /S  "../lib/Release" 
+cmake ../ -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 16 2019" -A x64 -DCMAKE_GENERATOR_PLATFORM=x64
+MSBuild log4z.sln /property:Configuration=Debug /property:Platform=x64
+mv ../lib/Debug/* ../lib/
+rd /Q /S  "../lib/Debug"
+cd ..
